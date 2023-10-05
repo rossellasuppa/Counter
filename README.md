e86abe4ecd90b4a41b8994221701cf54c76ee621
Rossella Suppa <rossella.suppa.91@gmail.com>
2023-10-05 19:05:06 +0200

    eliminati elementi in html e creati attraverso js

---
+++ README.md
@@ -0,0 +1,9 @@
+# Counter
+This application allows the user to increase and decrease a counter value. 
+
+## Accessibility
+To use the counter, just open the "index.html" file with any browser application.
+
+## Other functionalities
+1. Reset Value
+2. Change counter color (you can set the color by pressing the most left button)
--- assets/js/file.js
+++ assets/js/file.js
@@ -1,34 +1,63 @@
 // Riferimenti agli elementi HTML
 const counterElement = document.getElementById("counter");
-const incrementButton = document.getElementById("increment");
-const decrementButton = document.getElementById("decrement");
-const resetButton = document.getElementById("resetButton");
-const changeColorButton = document.getElementById("changeColorButton");
-const colorPicker = document.getElementById("colorPicker");
 
+
+// Creazione bottoni e Eventi di incremento e decremento
+const decrementButton = document.createElement("button");
+counterElement.appendChild (decrementButton);
+decrementButton.textContent = "-";
+decrementButton.id = "counterButton";
+decrementButton.addEventListener("click", () => {
+    counterValue--;
+    updateCounter();
+});
+
+const counterValueBox = document.createElement("div");
 let counterValue = 0;
-counterElement.textContent = counterValue;
+counterValueBox.textContent = counterValue;
+counterValueBox.id = "value";
+counterElement.appendChild(counterValueBox);
 
-// Funzione per aggiornare il colore dei pulsanti e del div
-function updateButtonAndDivColor(color) {
-    incrementButton.style.backgroundColor = color;
-    decrementButton.style.backgroundColor = color;
-    resetButton.style.backgroundColor = color;
-    counterElement.style.color = color;
-    changeColorButton.style.backgroundColor = color;
-    updateContainerBorder(color);
+const incrementButton = document.createElement ("button")
+incrementButton.textContent = "+";
+incrementButton.id = "counterButton";
+counterElement.appendChild (incrementButton);
+
+incrementButton.addEventListener("click", () => {
+    counterValue++;
+    updateCounter();
+});
+
+//Evento di aggiornamento del counter
+function updateCounter() {
+    counterValueBox.textContent = counterValue;
 }
 
-// Funzione che permette di cambiare anche il bordo del div genitore (container in html/css)
-function updateContainerBorder(color) {
-    const container = document.querySelector(".container");
-    container.style.borderColor = color;
-}
+//creazione di un div per i bottoni aggiuntivi
+const functionsBox = document.createElement("div");
+document.body.appendChild(functionsBox);
+functionsBox.id = "buttonContainer";
 
-// Imposta il colore predefinito all'avvio
-updateButtonAndDivColor(colorPicker.value);
-const defaultColor = colorPicker.value;
-updateButtonAndDivColor(defaultColor);
+//creazione di input, bottone Cambio colore e reset
+const colorPicker = document.createElement("input");
+colorPicker.id = "colorPicker";
+colorPicker.setAttribute ("type" , "color");
+colorPicker.setAttribute ("value" , "#007BFF");
+functionsBox.appendChild (colorPicker)
+
+const changeColorButton = document.createElement("button");
+functionsBox.appendChild(changeColorButton);
+changeColorButton.textContent = "Change color";
+changeColorButton.id = "functionsButton";
+changeColorButton.addEventListener("click", () => {
+    const selectedColor = colorPicker.value;
+    updateButtonAndDivColor(selectedColor);
+});
+
+const resetButton = document.createElement("button");
+functionsBox.appendChild(resetButton);
+resetButton.id = "functionsButton";
+resetButton.textContent = "Reset Value";
 
 // Evento che fa cambiare colore quando il pulsante "Change Color" viene premuto
 changeColorButton.addEventListener("click", () => {
@@ -36,16 +65,20 @@ changeColorButton.addEventListener("click", () => {
     updateButtonAndDivColor(selectedColor);
 });
 
-// Eventi di incremento e decremento
-incrementButton.addEventListener("click", () => {
-    counterValue++;
-    updateCounter();
-});
+function updateButtonAndDivColor(color) {
+    incrementButton.style.backgroundColor = color;
+    decrementButton.style.backgroundColor = color;
+    resetButton.style.backgroundColor = color;
+    counterValueBox.style.color = color;
+    changeColorButton.style.backgroundColor = color;
+    updateContainerBorder(color);
+}
 
-decrementButton.addEventListener("click", () => {
-    counterValue--;
-    updateCounter();
-});
+// Funzione che permette di cambiare anche il bordo del div genitore (container in html/css)
+function updateContainerBorder(color) {
+    const container = document.getElementById("counter");
+    container.style.borderColor = color;
+}
 
 // Evento di reset
 resetButton.addEventListener("click", function() {
@@ -53,9 +86,6 @@ resetButton.addEventListener("click", function() {
     updateCounter();
 });
 
-function updateCounter() {
-    counterElement.textContent = counterValue;
-}
 
 
 
